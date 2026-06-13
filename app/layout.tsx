94698b55bee99b993db17ce7ee55f38a0e119e1b
engr-idemudia <idemudiamondayosaghae@gmail.com>
2026-06-13 12:59:20 +0300

    fix: force dark theme on auth pages

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,6 +33,7 @@ export default function RootLayout({
           attribute="class"
           defaultTheme="dark"
           enableSystem={false}
+          disableTransitionOnChange
         >
           {children}
         </ThemeProvider>
